04127d50208d9e71155ee1cc175b739e8425c0cd
Advaith Suresh <advaithsuresh23@gmail.com>
2022-10-11 15:16:44 +1000

    Arduino code reads five inputs

--- MaxMSP/ReadInput/ReadInput.ino
+++ MaxMSP/ReadInput/ReadInput.ino
@@ -1,4 +1,8 @@
-int knobValue = 0;
+int knobValue0 = 0;
+int knobValue1 = 0;
+int knobValue2 = 0;
+int knobValue3 = 0;
+int knobValue4 = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -6,8 +10,23 @@ void setup() {
 }
 
 void loop() {
-  knobValue = analogRead(0);
-  Serial.println(knobValue);
-
+  knobValue0 = analogRead(0);
+  knobValue1 = analogRead(1);
+  knobValue1 = analogRead(2);
+  knobValue1 = analogRead(3);
+  knobValue1 = analogRead(4);
+     
+  Serial.print(knobValue0);
+  Serial.print(" ");
+  Serial.print(knobValue1);
+  Serial.print(" ");
+  Serial.print(knobValue2);
+  Serial.print(" ");
+  Serial.print(knobValue3);
+  Serial.print(" ");
+  Serial.print(knobValue4);
+  Serial.println();
+  
+  
   delay(5);
 }
