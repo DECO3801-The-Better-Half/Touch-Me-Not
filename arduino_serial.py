144b75df1f38b574a57af49993a76ed2b9f603b8
Sam Lucena <samlucena2@gmail.com>
2022-10-16 18:44:17 +1000

    Update Arduino serial to handle all serial

--- arduino_serial.py
+++ arduino_serial.py
@@ -3,33 +3,41 @@
 Class to parse the input from the arduino serial.
 """
 
-from typing import List, Dict
+import serial
+from typing import List, Dict, Optional
+
+BOARD_RATE = 9600
 
 
 class ArduinoSerial:
     """Class to parse input from serial"""
 
-    def __init__(self, order: List[str]):
+    def __init__(self, order: List[str], port: str):
         """Set the order of the instruments in serial inputs
 
         Parameters:
             order: the instrument names, in order of appearance
+            port: port name
         """
         # Expected order: (left instrument first, right instruments second)
         # l_lamp, l_flower, l_dragonfly,l_plant2, l_plant1, r_lamp, r_water,
         # r_plant2, r_plant1, r_flower
         self._order = order
+        self._port = port
+        self._ser = serial.Serial(port, BOARD_RATE, timeout=1)
 
-    def parse_serial(self, serial: bytes) -> Dict[str, int]:
-        """Return a dictionary mapping instrument name -> value
-
-        Parameters:
-            serial: serial input from the arduino
+    def get_serial(self) -> Optional[Dict[str, int]]:
+        """Return a dictionary mapping instrument name -> value from serial
 
         Returns:
-            dictionary mapping instrument names to values
+            dictionary mapping instrument names to instrument values or None if
+            serial has nothing to read
         """
-        values = serial.decode().strip().split(" ")
+        if not self._ser.inWaiting():
+            return None
+
+        line = self._ser.readline()
+        values = line.decode().strip().split(" ")
         result = {}
 
         for i in range(len(values)):
