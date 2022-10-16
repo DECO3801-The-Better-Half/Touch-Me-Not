d4128b1e8477672898747299b77404ff8a78f5d7
Lauren Whitehead <lauren.whitehead@outlook.com>
2022-10-16 11:50:16 +1000

    Add and scale LED controller functionality

--- LED_controller/left_LED_controller/left_LED_controller.ino
+++ LED_controller/left_LED_controller/left_LED_controller.ino
@@ -17,27 +17,31 @@ int thresholds[] = {1000, 1000, 1000, 1000, 800};
 long values[4];
 
 //define colour varibles using RGB values
-uint32_t pink = strip.Color(242, 90, 186);
-uint32_t purple = strip.Color(144, 50, 206);
+uint32_t pink = strip.Color(255, 90, 186);
+uint32_t purple = strip.Color(144, 0, 206);
 uint32_t green = strip.Color(0,255, 0);
 uint32_t blue = strip.Color(0, 0, 255);
 uint32_t yellow = strip.Color(255, 211, 30);
 uint32_t none = strip.Color(0, 0, 0);
 
 //define each object's LEDs that light up when it is tocuhed
-int plant1[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23};
-int plant2[] = {24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47};
-int flower[] = {48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71};
-int water [] = {72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95};
+// int plant1[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23};
+// int plant2[] = {24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47};
+// int flower[] = {48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71};
+// int dragonfly [] = {72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95};
 int lamp[] = {96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119};
 
+int plant1[] = {0,1,2,3,16,17,18,19,32,33,34,35,48,49,50,51,64,65,66,67,80,81,82,83};
+int plant2[] = {4,5,6,7,20,21,22,23,36,37,38,39,52,53,54,55,68,69,70,71,84,85,86,87};
+int flower[] = {8,9,10,11,24,25,26,27,40,41,42,43,56,57,58,59,72,73,74,75,88,89,90,91};
+int dragonfly [] = {12,13,14,15,28,29,30,31,44,45,46,47,60,61,62,63,76,77,78,79,92,93,94,95};
 
 void setup() {
-Serial.begin(9600);
-strip.begin(); //always needed
-strip.show(); // 0 data => off.
-strip.setBrightness(125); // ~50%
-int n = strip.numPixels();
+  Serial.begin(9600);
+  strip.begin(); //always needed
+  strip.show(); // 0 data => off.
+  strip.setBrightness(255); // ~50%
+  int n = strip.numPixels();
 }
 
 void loop() {
@@ -57,8 +61,8 @@ led_response(values[1], thresholds[1], plant2, purple);
 //flower LED response
 led_response(values[2], thresholds[2], flower, green);
 
-//water LED response
-led_response(values[3], thresholds[3], water, blue);
+//dragonfly LED response
+led_response(values[3], thresholds[3], dragonfly, blue);
 
 //lamp LED response
 led_response(analogRead(lampPin), thresholds[4], lamp, yellow);
